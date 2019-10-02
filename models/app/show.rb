24afed0025519c6e81b90cd9d668ad288e776d2d
Randy Taylor <randyisbusy@gmail.com>
2019-10-02 07:40:55 +0000

    Automatically backed up by Learn

--- models/app/show.rb
+++ models/app/show.rb
@@ -4,7 +4,7 @@ class Show <  ActiveRecord::Base
   end
   def self.most_popular_show
     x = Show.maximum(:rating)
-    y = Show.find_by :rating == 10
+    y = Show.find_by :rating == x
     y
 end
 end
