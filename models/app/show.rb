c5f8068333319fa8126db6d5cc2377cc7c8518e1
Randy Taylor <randyisbusy@gmail.com>
2019-10-02 07:23:56 +0000

    Automatically backed up by Learn

--- models/app/show.rb
+++ models/app/show.rb
@@ -4,7 +4,7 @@ class Show <  ActiveRecord::Base
   end
   def self.most_popular_show
     x = self.maximum(:rating)
-    y = self.name.where(":rating == #{x}")
+    y = self.name.when(":rating == #{x}")
 
     y
 end
