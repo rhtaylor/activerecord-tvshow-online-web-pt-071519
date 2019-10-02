44f5e463b8bea5ca35c23b993e7bbef3ca85df57
Randy Taylor <randyisbusy@gmail.com>
2019-10-02 07:12:05 +0000

    Automatically backed up by Learn

--- models/app/show.rb
+++ models/app/show.rb
@@ -2,5 +2,7 @@ class Show <  ActiveRecord::Base
   def self.highest_rating
     self.maximum(:rating)
   end
-
+  def self.most_popular_show
+    x = self.find_by(Show.maximum(:rating))
+    binding.pry
 end
